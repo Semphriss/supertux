a576579972f389f9337166c0679728545f532072
Semphris <semphris@protonmail.com>
2021-01-28 23:50:54 -0500

    Added TopBar component

---
+++ src/editor/topbar_widget.hpp
@@ -0,0 +1,50 @@
+//  SuperTux
+//  Copyright (C) 2020 A. Semphris <semphris@protonmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HEADER_SUPERTUX_EDITOR_TOPBAR_WIDGET_HPP
+#define HEADER_SUPERTUX_EDITOR_TOPBAR_WIDGET_HPP
+
+#include "editor/widget.hpp"
+
+/** The toolbox is on the right side of the screen and allows
+    selection of the current tool and contains the object or tile
+    palette */
+class EditorTopbarWidget final : public Widget
+{
+public:
+  EditorTopbarWidget();
+
+  virtual void draw(DrawingContext& context) override;
+  virtual void update(float dt_sec) override;
+
+  virtual bool on_mouse_button_up(const SDL_MouseButtonEvent& button) override;
+  virtual bool on_mouse_button_down(const SDL_MouseButtonEvent& button) override;
+  virtual bool on_mouse_motion(const SDL_MouseMotionEvent& motion) override;
+  virtual bool on_mouse_wheel(const SDL_MouseWheelEvent& wheel) override;
+  virtual bool on_key_up(const SDL_KeyboardEvent& key) override;
+  virtual bool on_key_down(const SDL_KeyboardEvent& key) override;
+
+  virtual void setup() override;
+  virtual void resize() override;
+
+private:
+  EditorTopbarWidget(const EditorTopbarWidget&) = delete;
+  EditorTopbarWidget& operator=(const EditorTopbarWidget&) = delete;
+};
+
+#endif
+
+/* EOF */
